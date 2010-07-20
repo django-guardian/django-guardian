d2b8cbe25ef12d7b3594d74d5207ce94e82ca7d6
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-07-20 21:50:41 +0200

    Slightly improved coverage script

--- run_test_and_report.sh
+++ run_test_and_report.sh
@@ -4,5 +4,5 @@ echo "Running test suite with coverage report at the end"
 echo -e "( would require coverage python package to be installed )\n"
 
 coverage run setup.py test
-coverage report -m --omit=example_project,setup,tests,guardian/tests,guardian/conf/settings
+coverage report -m guardian/*.py
 
