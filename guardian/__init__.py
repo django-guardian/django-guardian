698b179d8fc738df445babf26224ce0734f31465
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2010-11-23 13:26:22 +0100

    Extended version number so 'pre' is not omitted

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -9,5 +9,5 @@ def get_version():
     """
     Returns shorter version (digit parts only) as string.
     """
-    return '.'.join((str(each) for each in VERSION[:3]))
+    return '.'.join((str(each) for each in VERSION[:4]))
 
