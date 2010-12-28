26ea59f56beb51f5b697064f5591c323e5df7b73
Christopher Petrilli <petrilli@amber.org>
2010-12-28 09:50:29 -0500

    Added support_inactive_user attribute to squelch DeprecationWarning in Django 1.3

--- guardian/backends.py
+++ guardian/backends.py
@@ -7,6 +7,7 @@ from guardian.core import ObjectPermissionChecker
 class ObjectPermissionBackend(object):
     supports_object_permissions = True
     supports_anonymous_user = True
+    supports_inactive_user = True
 
     def authenticate(self, username, password):
         return None
