5bedbe9d0a171163f8a5a6be22e79eac1ab07f95
Lukasz Balcerzak <lukasz.balcerzak@python-center.org>
2011-01-14 01:10:34 +0100

    Version bump

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -1,7 +1,7 @@
 """
 Implementation of per object permissions for Django 1.2.
 """
-VERSION = (1, 0, 0, 'beta2')
+VERSION = (1, 0, 0, 'rc1')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
