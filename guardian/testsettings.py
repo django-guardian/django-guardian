2bee4a1e4b82514f8650aeaf5dc3634e354024a8
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-02-20 11:33:17 +0100

    Added py33-django15 to default tox's envlist

--- guardian/testsettings.py
+++ guardian/testsettings.py
@@ -39,7 +39,7 @@ TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), 'tests', 'templates'),
 )
 
-SECRET_KEY = ''.join([random.choice(string.letters) for x in range(40)])
+SECRET_KEY = ''.join([random.choice(string.ascii_letters) for x in range(40)])
 
 # Database specific
 
