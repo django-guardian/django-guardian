8d8400680b198cf3c9f828f6c494193a820f83e8
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-02-27 00:05:58 +0100

    Bumped version

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -3,7 +3,7 @@ Implementation of per object permissions for Django 1.2 or later.
 """
 from __future__ import unicode_literals
 
-VERSION = (1, 1, 0, 'beta')
+VERSION = (1, 1, 0, 'dev')
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
