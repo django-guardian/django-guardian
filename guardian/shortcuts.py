930d07e8d19973be6c1f7d56a3df49767e53cb52
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-09-20 18:29:30 +0200

    Fixed flakes

--- guardian/shortcuts.py
+++ guardian/shortcuts.py
@@ -39,7 +39,7 @@ def assign_perm(perm, user_or_group, obj=None):
     We can assign permission for ``Model`` instance for specific user:
 
     >>> from django.contrib.sites.models import Site
-    >>> from guardian.models import User, Group
+    >>> from guardian.models import User
     >>> from guardian.shortcuts import assign_perm
     >>> site = Site.objects.get_current()
     >>> user = User.objects.create(username='joe')
@@ -250,7 +250,7 @@ def get_groups_with_perms(obj, attach_perms=False):
         >>>
         >>> page = FlatPage.objects.create(title='Some page', path='/some/page/')
         >>> admins = Group.objects.create(name='Admins')
-        >>> assign_perm('change_flatpage', group, page)
+        >>> assign_perm('change_flatpage', admins, page)
         >>>
         >>> get_groups_with_perms(page)
         [<Group: admins>]
@@ -307,6 +307,7 @@ def get_objects_for_user(user, perms, klass=None, use_groups=True, any_perm=Fals
 
     Example::
 
+        >>> from django.contrib.auth.models import User
         >>> from guardian.shortcuts import get_objects_for_user
         >>> joe = User.objects.get(username='joe')
         >>> get_objects_for_user(joe, 'auth.change_group')
