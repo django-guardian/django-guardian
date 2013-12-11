c853b82d8bda4b470d66649ff57e03ed368ecea1
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-12-12 00:22:34 +0100

    Updated integration test
    we already expect example project to be run with
    Django 1.5+

--- example_project/integration_tests/tests.py
+++ example_project/integration_tests/tests.py
@@ -1,7 +1,4 @@
-from django import VERSION
 from django.test import TestCase
-from django.test.utils import override_settings
-from django.utils.unittest import skipUnless
 
 from guardian.compat import get_user_model
 from guardian.management import create_anonymous_user
@@ -10,9 +7,10 @@ from guardian.management import create_anonymous_user
 User = get_user_model()
 
 
-@skipUnless(VERSION[:3] >= (1, 5, 0), 'Previous Django version do not support \
-        custom models')
 class CustomUserTests(TestCase):
     def test_create_anonymous_user(self):
         create_anonymous_user(object())
         self.assertEqual(1, User.objects.all().count())
+        anonymous = User.objects.all()[0]
+        self.assertTrue(anonymous.is_anonymous())
+
