c63857d6fc5f0171cbb8180e6b12189c9204f698
Jay McEntire <jay.mcentire@gmail.com>
2014-05-23 09:54:16 -0600

    Changes for get_objects_for_user
    Also need to cast to list onvalues so data types work right.

--- guardian/shortcuts.py
+++ guardian/shortcuts.py
@@ -430,10 +430,10 @@ def get_objects_for_user(user, perms, klass=None, use_groups=True, any_perm=Fals
         counts = user_obj_perms_queryset.values(fields[0]).annotate(object_pk_count=Count(fields[0]))
         user_obj_perms_queryset = counts.filter(object_pk_count__gte=len(codenames))
 
-    values = user_obj_perms_queryset.values_list(fields[0], flat=True)
+    values = list(user_obj_perms_queryset.values_list(fields[0], flat=True))
     objects = queryset.filter(pk__in=values)
     if use_groups:
-        values = groups_obj_perms_queryset.values_list(fields[0], flat=True)
+        values = list(groups_obj_perms_queryset.values_list(fields[0], flat=True))
         objects |= queryset.filter(pk__in=values)
 
     return objects
