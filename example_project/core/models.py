a38195658efcaffcbb74a596819159da2f234747
Thanh Lê <lethanhx2k@gmail.com>
2014-09-16 14:30:37 +0700

    Commented out USERNAME_FIELD
    This caused error: TypeError: create_superuser() takes exactly 4 arguments (3 given)
    I removed this line (following the solution here http://stackoverflow.com/questions/17342702/cant-create-superuser-with-custom-userprofile), it works.

--- example_project/core/models.py
+++ example_project/core/models.py
@@ -7,7 +7,7 @@ class CustomUser(AbstractUser):
     real_username = models.CharField(max_length=120, unique=True)
     birth_date = models.DateField(null=True, blank=True)
 
-    USERNAME_FIELD = 'real_username'
+    #USERNAME_FIELD = 'real_username'
 
     def save(self, *args, **kwargs):
         if not self.real_username:
