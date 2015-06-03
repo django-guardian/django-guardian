9a27738a2a694c7033e7a396aa3b118e02126790
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2015-06-03 10:34:05 +0200

    Freezying v1.3

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -3,7 +3,7 @@ Implementation of per object permissions for Django 1.2 or later.
 """
 from __future__ import unicode_literals
 
-VERSION = (1, 2, 6, 'dev')
+VERSION = (1, 3)
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
