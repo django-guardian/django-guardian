675f1d28389b160b394754567f1a861aaf492dc3
Brian May <brian@linuxpenguins.xyz>
2015-11-13 15:14:46 +1100

    Migrations for Django 1.8
    Nothing should break for Django < 1.7 as these migrations aren't used.
    
    Not included one migration which breaks under Django 1.7:
    
    migrations.AlterModelManagers(
        name='customuser',
        managers=[
            ('objects', django.contrib.auth.models.UserManager()),
        ],
    ),

---
+++ guardian/testapp/migrations/0004_auto_20151112_2209.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.core.validators
+import django.contrib.auth.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('testapp', '0003_auto_20141124_0729'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='customuser',
+            name='email',
+            field=models.EmailField(max_length=254, verbose_name='email address', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='customuser',
+            name='groups',
+            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups'),
+        ),
+        migrations.AlterField(
+            model_name='customuser',
+            name='last_login',
+            field=models.DateTimeField(null=True, verbose_name='last login', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='customuser',
+            name='username',
+            field=models.CharField(error_messages={'unique': 'A user with that username already exists.'}, max_length=30, validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, verbose_name='username'),
+        ),
+    ]
