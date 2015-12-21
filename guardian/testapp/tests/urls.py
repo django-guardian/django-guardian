f0041855d5a81f9e39648f2f0d68461ae8fd5927
Wtower <gkarak@9-dev.com>
2015-12-21 12:49:35 +0200

    Fix RemovedInDjango110Warning: "django.conf.urls.patterns() is deprecated and will be removed in Django 1.10. Update your urlpatterns to be a list of django.conf.urls.url() instances instead." Dropped support for Django < 1.7

--- guardian/testapp/tests/urls.py
+++ guardian/testapp/tests/urls.py
@@ -1,25 +1,19 @@
 from __future__ import unicode_literals
 # handler404 and handler500 are needed for admin tests
-from guardian.compat import include, patterns, handler404, handler500 # pyflakes:ignore
+from guardian.compat import include, url, handler404, handler500  # pyflakes:ignore
 from guardian.mixins import PermissionRequiredMixin
 from django.contrib import admin
-from django import get_version as django_get_version
-
-if (django_get_version() >= "1.5"):
-    from django.views.generic import View
+from django.contrib.auth.views import login
+from django.views.generic import View
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-)
 
-if (django_get_version() >= "1.5"):
+class TestClassRedirectView(PermissionRequiredMixin, View):
+    permission_required = 'testapp.change_project'
 
-    class testClassRedirectView(PermissionRequiredMixin, View):
-        permission_required = 'testapp.change_project'
-
-    urlpatterns += patterns('',
-        (r'^accounts/login/', 'django.contrib.auth.views.login', {'template_name': 'blank.html'}),
-        (r'^permission_required/', testClassRedirectView.as_view()),
-    )
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/', login, {'template_name': 'blank.html'}),
+    url(r'^permission_required/', TestClassRedirectView.as_view()),
+]
