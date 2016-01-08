80ded8cb7eedde85f96420a8645f684fb7ece16c
Brian May <brian@linuxpenguins.xyz>
2016-01-08 19:12:03 +1100

    Release version 1.4.0

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -4,7 +4,7 @@ Implementation of per object permissions for Django 1.2 or later.
 from __future__ import unicode_literals
 from . import checks
 
-VERSION = (1, 3, 2)
+VERSION = (1, 4, 0)
 
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 
