f84c2d46df87f2c8eae01db8d247aa6c8b1b2362
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-02-09 04:57:54 +0100

    Drop django.conf.urls.patterns (django 1.10 compat)
    See https://github.com/django/django/blob/stable/1.9.x/django/conf/urls/__init__.py#L80-L85

--- example_project/posts/urls.py
+++ example_project/posts/urls.py
@@ -1,8 +1,8 @@
-from guardian.compat import url, patterns
+from guardian.compat import url
 
 
-urlpatterns = patterns(
+urlpatterns = [
     'posts.views',
     url(r'^$', view='post_list', name='posts_post_list'),
     url(r'^(?P<slug>[-\w]+)/$', view='post_detail', name='posts_post_detail'),
-)
+]
