79280c297e660c23b5694ec382a22131e943ae0c
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-02-09 05:06:55 +0100

    Add modern settings for templates engine (django 1.10 compat)
    See https://docs.djangoproject.com/en/1.9/ref/templates/upgrading/

--- guardian/testapp/testsettings.py
+++ guardian/testapp/testsettings.py
@@ -50,3 +50,23 @@ SECRET_KEY = ''.join([random.choice(string.ascii_letters) for x in range(40)])
 # Database specific
 
 DATABASES = {'default': env.db(default="sqlite:///")}
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': TEMPLATE_DIRS,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
