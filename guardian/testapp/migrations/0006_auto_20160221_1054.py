33a4a059cd8c5d8e5a35a71fefdf3373380df09b
Rolf Håvard Blindheim <rhblind@gmail.com>
2016-02-21 18:28:38 +0100

    Remove everything from migrations that has nothing to do with `CustomUsernameUser`

--- guardian/testapp/migrations/0006_auto_20160221_1054.py
+++ guardian/testapp/migrations/0006_auto_20160221_1054.py
@@ -26,29 +26,5 @@ class Migration(migrations.Migration):
                 'abstract': False,
             },
             bases=(models.Model, guardian.mixins.GuardianUserMixin),
-        ),
-        migrations.AlterField(
-            model_name='customuser',
-            name='email',
-            field=models.EmailField(max_length=75, verbose_name='email address', blank=True),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='customuser',
-            name='groups',
-            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', verbose_name='groups'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='customuser',
-            name='last_login',
-            field=models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='customuser',
-            name='username',
-            field=models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, max_length=30, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')]),
-            preserve_default=True,
-        ),
+        )
     ]
