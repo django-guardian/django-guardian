7501edc1e0180460eb7b4fc4deb3314e814f36e6
Ben Konrath <ben@bagu.org>
2016-06-22 11:14:16 +0200

    Add docs, add tests and remove unneeded function.

--- guardian/ctypes.py
+++ guardian/ctypes.py
@@ -14,36 +14,3 @@ def get_content_type(obj):
 
 def get_default_content_type(obj):
     return ContentType.objects.get_for_model(obj)
-
-
-#
-# Just for example. Will not be included with django-guardian.
-#
-def get_polymorphic_base_content_type(obj):
-    """
-    Special helper function to return BASE polymorphic ctype id
-    """
-    if hasattr(obj, 'polymorphic_model_marker'):
-        try:
-            superclasses = list(obj.__class__.mro())
-        except TypeError:
-            # obj is an object so mro() need to be called with the obj.
-            superclasses = list(obj.__class__.mro(obj))
-
-        polymorphic_superclasses = list()
-        for sclass in superclasses:
-            if hasattr(sclass, 'polymorphic_model_marker'):
-                polymorphic_superclasses.append(sclass)
-
-        # PolymorphicMPTT adds an additional class between polymorphic and
-        # base class
-        if hasattr(obj, 'can_have_children'):
-            root_polymorphic_class = polymorphic_superclasses[-3]
-        else:
-            root_polymorphic_class = polymorphic_superclasses[-2]
-        ctype = ContentType.objects.get_for_model(root_polymorphic_class)
-
-    else:
-        ctype = ContentType.objects.get_for_model(obj)
-
-    return ctype
