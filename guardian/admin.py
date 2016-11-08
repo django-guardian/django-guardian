c86eaf0d7a930dd0b1d598f7fd08760ab465c844
Michael Lelyakin <m.leliykin@just-work.org>
2016-11-08 22:36:55 +0300

    Remove unused imports and refactor
    I remove unused imports from compat.py file

--- guardian/admin.py
+++ guardian/admin.py
@@ -1,13 +1,15 @@
 from __future__ import unicode_literals
+from collections import OrderedDict
 from django import forms
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin, messages
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.shortcuts import get_object_or_404, redirect, render_to_response, render
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext
-from guardian.compat import get_model_name, get_user_model, OrderedDict, url, reverse
+from guardian.compat import get_model_name, get_user_model, reverse
 from guardian.forms import GroupObjectPermissionsForm, UserObjectPermissionsForm
 from guardian.models import Group
 from guardian.shortcuts import (get_group_perms, get_groups_with_perms, get_perms_for_model, get_user_perms,
