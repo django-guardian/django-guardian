90a492e1d5150570b0921fa703194e8c0ab4873d
Michael Käufl <django-guardian@c.michael-kaeufl.de>
2017-05-19 18:53:50 +0200

    [example_project] Do not wrap admin urls in include
    `admin.site.urls` is a 3-tuple [1], but `include` does not allow this
    any more.  This fixes a RemovedInDjango20Warning.
    
    [1] https://github.com/django/django/blob/99df304c856ac2f6ee18418ea8ca3e2dc7b47dac/django/contrib/admin/sites.py#L277-L279
    [2] https://github.com/django/django/blob/99df304c856ac2f6ee18418ea8ca3e2dc7b47dac/django/conf/urls/__init__.py#L29-L33

--- example_project/urls.py
+++ example_project/urls.py
@@ -9,7 +9,7 @@ __all__ = ['handler404', 'handler500']
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
     url(r'^article/', include('articles.urls', namespace='articles')),
     url(r'^', include('posts.urls')),
