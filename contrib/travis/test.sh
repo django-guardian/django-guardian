7bfe89e40c886f17509d2fc57d7df3e2ae018109
Adam Dobrawy <ad-m@users.noreply.github.com>
2017-12-03 06:04:01 +0100

    Add django 2.0 to test matrix & update docs  (#537)
    * Add Django 2.0 to test matrix in tox.ini and TravisCI
    * Add Django 2.0 to setup.py classifiers
    * Update docs about django 2.0 support

---
+++ contrib/travis/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+python ./setup.py --version
+py.test --cov=guardian
+
+# Test example_project on supported django versions
+if [ "$DJANGO_VERSION" = "1.10" ] || \
+   [ "$DJANGO_VERSION" = "1.11" ] || \
+   [ "$DJANGO_VERSION" = "2.0" ] || \
+   [ "$DJANGO_VERSION" = "master" ]; then
+    pip install .; 
+    cd example_project; 
+    python manage.py test;
+fi;
