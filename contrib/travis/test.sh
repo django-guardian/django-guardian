5ca0b3fb863b615fed9845413b65eb40d4678c45
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-12-03 05:13:05 +0100

    Fix shell compatibility in test.sh

--- contrib/travis/test.sh
+++ contrib/travis/test.sh
@@ -4,7 +4,10 @@ python ./setup.py --version
 py.test --cov=guardian
 
 # Tests django example on supported;
-if [[ $DJANGO_VERSION = 1.10 || $DJANGO_VERSION = 1.11 || $DJANGO_VERSION = 2.0 || $DJANGO_VERSION = "master" ]]; then
+if [ "$DJANGO_VERSION" = "1.10" ] || \
+   [ "$DJANGO_VERSION" = "1.11" ] || \
+   [ "$DJANGO_VERSION" = "2.0" ] || \
+   [ "$DJANGO_VERSION" = "master" ]; then
     pip install .; 
     cd example_project; 
     python manage.py test;
