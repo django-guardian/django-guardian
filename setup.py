ede86bf9a05b1db23b6b95360cc46883f573d84f
Michael Käufl <django-guardian@c.michael-kaeufl.de>
2018-09-17 13:24:42 +0200

    Add support for Python 3.7
    Regards django-guardian/django-guardian#582

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
                  'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
                  ],
     test_suite='tests.main',
     cmdclass={'flakes': RunFlakesCommand},
