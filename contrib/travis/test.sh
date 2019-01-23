e83d03712e8252c3398d62031d24e91976100171
Michael Käufl <django-guardian@c.michael-kaeufl.de>
2019-01-23 22:34:26 +0100

    [travis] Run tests against latest patch releases of django

--- contrib/travis/test.sh
+++ contrib/travis/test.sh
@@ -4,9 +4,9 @@ python ./setup.py --version
 py.test --cov=guardian
 
 # Test example_project on supported django versions
-if [ "$DJANGO_VERSION" = "1.11" ] || \
-   [ "$DJANGO_VERSION" = "2.0" ] || \
-   [ "$DJANGO_VERSION" = "2.1" ] || \
+if [ "${DJANGO_VERSION:0:3}" = "1.11" ] || \
+   [ "${DJANGO_VERSION:0:3}" = "2.0" ] || \
+   [ "${DJANGO_VERSION:0:3}" = "2.1" ] || \
    [ "${DJANGO_VERSION:0:3}" = "2.2" ] || \
    [ "$DJANGO_VERSION" = "master" ]; then
     pip install .; 
