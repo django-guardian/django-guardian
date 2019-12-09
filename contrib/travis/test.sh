865d430180948d681ca84aa0d45558d90939b05f
l0rb <lorbritzer@yahoo.de>
2019-12-09 15:38:25 +0100

    turn warnings during tests into exceptions

--- contrib/travis/test.sh
+++ contrib/travis/test.sh
@@ -15,5 +15,5 @@ if [ "${DJANGO_VERSION:0:3}" = "2.1" ] || \
     pip install .;
     cd example_project;
     python -Wa manage.py makemigrations --check --dry-run;
-    python -Wa manage.py test;
+    python -We manage.py test;
 fi;
