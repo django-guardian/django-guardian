f88d19079087e19ac36e1878b50b98b5cee32214
partimer <37935350+partimer@users.noreply.github.com>
2019-12-10 10:19:59 -0500

    defer get obj perms model
    Defer get obj perms model call until runtime to avoid potential circular dependencies

--- guardian/models/__init__.py
+++ guardian/models/__init__.py
@@ -9,13 +9,6 @@ from .models import (
     Group
 )
 
-# Must import after .models
-# When .models is loaded, default generic object permissions are created
-# The following statements may redirect external references to custom
-# generic object permission models
-from guardian.utils import get_user_obj_perms_model, get_group_obj_perms_model
-UserObjectPermission = get_user_obj_perms_model()
-GroupObjectPermission = get_group_obj_perms_model()
 
 __all__ = [
     'BaseObjectPermission',
