96372b3370eef1c5d8ca53bc3a6b53db0a06b082
Michael Käufl <django-guardian@c.michael-kaeufl.de>
2020-04-14 10:50:56 +0200

    Run migrations check only if required version of django is installed
    Django master changed the `max_length` of `AbstractUser`'s `first_name`.
    
    See
    - https://github.com/django/django/commit/5f8495a40ab1554e81ac845484da890dd390e1d8
    - https://github.com/django-guardian/django-guardian/commit/88b8d41ada5de7db2452d67e28955332bf48ecf8

--- contrib/travis/test.sh
+++ contrib/travis/test.sh
@@ -11,5 +11,11 @@ py.test --cov=guardian
 # Test example_project
 pip install .;
 cd example_project;
-python -Wa manage.py makemigrations --check --dry-run;
+EXAMPLE_PROJECTS_DJANGO_VERSION=$(grep -i "django[^-]" requirements.txt | cut -d "=" -f 2)
+if [ "${DJANGO_VERSION:0:3}" = "${EXAMPLE_PROJECTS_DJANGO_VERSION:0:3}" ]; then
+    python -Wa manage.py makemigrations --check --dry-run;
+elif [ -z "${EXAMPLE_PROJECTS_DJANGO_VERSION}" ]; then
+    echo "Could not determine which version of Django the example project supports."
+    exit 1
+fi;
 python -Wa manage.py test;
