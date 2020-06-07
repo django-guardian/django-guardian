e86e8f601683042682a3758f2379584fca33075f
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-06-08 01:51:08 +0200

    Bump version to 2.3.0

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -6,7 +6,7 @@ from . import checks
 default_app_config = 'guardian.apps.GuardianConfig'
 
 # PEP 396: The __version__ attribute's value SHOULD be a string.
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 
 # Compatibility to eg. django-rest-framework
 VERSION = tuple(int(x) for x in __version__.split('.')[:3])
