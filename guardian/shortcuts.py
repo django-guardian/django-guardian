61910d385ec65b0b8f5c8b86c1fc1d25a151e3af
Mark Edwards <me@g18e.com>
2020-12-18 18:57:30 +0000

    Respect GUARDIAN_GET_CONTENT_TYPE during ctype match check in guardian.shortcuts.get_objects_for_user

--- guardian/shortcuts.py
+++ guardian/shortcuts.py
@@ -530,7 +530,7 @@ def get_objects_for_user(user, perms, klass=None, use_groups=True, any_perm=Fals
         raise WrongAppError("Cannot determine content type")
     else:
         queryset = _get_queryset(klass)
-        if ctype.model_class() != queryset.model:
+        if ctype != get_content_type(queryset.model):
             raise MixedContentTypeError("Content type for given perms and "
                                         "klass differs")
 
