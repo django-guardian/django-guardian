5d44450cd61af25e452e6c807f85173de8bbc543
Rainshaw <rxg@live.com>
2021-03-01 22:18:37 +0800

    fix: register_variable test

--- guardian/testapp/tests/test_conf.py
+++ guardian/testapp/tests/test_conf.py
@@ -22,7 +22,7 @@ class TestConfiguration(TestCase):
         with mock.patch('guardian.conf.settings.REGISTER_VARIABLE', True):
             guardian_settings.register_variable()
             from django.conf import settings
-            self.assertEqual(settings.ANONYMOUS_USER_NAME, 'ANONYMOUS_USER_NAME')
+            self.assertEqual(settings.ANONYMOUS_USER_NAME, 'AnonymousUser')
 
 
 def get_test_content_type(obj):
