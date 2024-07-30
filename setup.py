f1176694fd97ef515ed0ece660f77fb7b55afa87
Zbigniew Skowron <zbigniew@precisionlife.com>
2024-07-30 10:54:04 +0200

    Fixes after review: - Removing downloadcache from tox.ini. - Replaced MD5PasswordHasher with PBKDF2PasswordHasher. - Removed duplicate "Python :: 3.9" in setup.py.

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
                  'Programming Language :: Python :: 3 :: Only',
                  'Programming Language :: Python :: 3.8',
                  'Programming Language :: Python :: 3.9',
-                 'Programming Language :: Python :: 3.9',
                  'Programming Language :: Python :: 3.10',
                  'Programming Language :: Python :: 3.11',
                  'Programming Language :: Python :: 3.12',
