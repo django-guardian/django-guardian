2c5bef79ddbb7514c0cd82639e5e2ee5167d4a7f
Tobias Funke <tfunke@bona-fide.it>
2024-10-31 16:12:55 +0100

    add changes from pyupgrade

--- example_project_custom_group/core/models.py
+++ example_project_custom_group/core/models.py
@@ -65,7 +65,7 @@ class CustomUser(AbstractBaseUser, CustomGroupPermissionsMixin, GuardianUserMixi
         """
         Return the first_name plus the last_name, with a space in between.
         """
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        full_name = '{} {}'.format(self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
