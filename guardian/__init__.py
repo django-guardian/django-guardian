d910b64cc38403a71a97edc824e4568b230d2e7d
Tom Clark <tom@octue.com>
2025-01-30 10:42:45 +0000

    OPS: Avoid using importlib on every package import
    BREAKING-CHANGE: Removed the __version__ and VERSION values from the __init__.py file. Anyone relying on these for direct runtime introspection should use the get_version() function instead.

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -2,19 +2,27 @@
 Implementation of per object permissions for Django.
 """
 from . import checks
-from importlib.metadata import version
 
-import re
-
-# Use importlib to ensure this version always matches with pyproject.toml
-__version__ = version('django-guardian')
-
-# Compatibility to eg. django-rest-framework
-# (removes any release candidate or other modifiers)
-VERSION = tuple(int(x) for x in re.split('[a-z]', __version__)[0].split('.')[:3])
 
 def get_version():
-    return __version__
+    """Return the version string (see pyproject.toml) of the package.
+
+    The value will comply with the [python version specifier format dicted by
+    PEP440](https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers)
+
+    Standards for packaging metadata — including version — are defined by PEP 621,
+    which specifies how to declare version in pyproject.toml.
+
+    The earlier PEP 396 suggests (but does not mandate) having a __version__ 
+    attribute in __init__.py for the purposes of runtime introspection, but
+    it leads to confusion in our development process to define it multiple places.
+
+    PEP 396 has now been revoked, but it is still useful to be able to inspect 
+    the package version at runtime. This function retains that ability using the
+    recommended importlib approach.
+    """
+    from importlib.metadata import version
+    return version('django-guardian')
 
 
 def monkey_patch_user():
